3c51d2e82680bed0d3324acd1c52d73ca3e888ac
JollyBatoms <jollycleff01@gmail.com>
2026-05-29 23:21:05 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase configuration — shared across all pages
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgtZvZzWdWQEJna0bJz_YhATLnnNoJRfA",
+  authDomain: "gecc-uniport.firebaseapp.com",
+  projectId: "gecc-uniport",
+  storageBucket: "gecc-uniport.firebasestorage.app",
+  messagingSenderId: "565729305146",
+  appId: "1:565729305146:web:2973acd0293903725acb03"
+};
+
+// Initialize Firebase (CDN version)
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
